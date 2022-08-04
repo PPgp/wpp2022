0b3dea6889df06fde0315c2014175bde6473ff3b
Hana Sevcikova <hanas@uw.edu>
2022-08-04 16:58:44 -0700

    mxB1 dataset added

---
+++ data/mxB1.R
@@ -0,0 +1,7 @@
+mxB1 <- local({
+    e <- new.env()
+    load("mx1dt.rda", envir= e)
+    load("countries.rda", envir= e)
+    e$mx1dt <- merge(data.table::as.data.table(e$mx1dt), e$countries, by = "country_code", sort = FALSE)
+    as.data.frame(data.table::dcast(e$mx1dt, country_order + country_code + name + age ~ year, value.var = "mxB")[, `:=`(country_order = NULL)][])
+})
