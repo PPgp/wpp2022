7294e925b2b74b2a989b53cdbdf3b98faaf7ba88
Hana Sevcikova <hanas@uw.edu>
2022-08-04 17:13:08 -0700

    added mxB to mx1dt

--- data-raw/create_mx_data.R
+++ data-raw/create_mx_data.R
@@ -11,10 +11,12 @@ source("functions.R")
 # annual estimates
 mxF <- data.table::fread('mxF.txt', sep = "\t")[, country := NULL]
 mxM <- data.table::fread('mxM.txt', sep = "\t")[, country := NULL]
+mxB <- data.table::fread('mxB.txt', sep = "\t")[, country := NULL]
 
 mx <- merge.with.countries(mxM)
 mx <- melt.and.merge(NULL, mx, "mxM", ids = c("country_order", "country_code", "age"))
 mx <- melt.and.merge(mx, mxF, "mxF", ids = c("country_code", "age"), by = c("country_code", "year", "age"))
+mx <- melt.and.merge(mx, mxB, "mxB", ids = c("country_code", "age"), by = c("country_code", "year", "age"))
 
 mx1dt <- mx[, year := as.integer(year)][order(country_order, year)][, country_order := NULL][]
 use_data(mx1dt, overwrite = TRUE)
