58afe9a55b2bb2a305a131c7947c973f080c972c
Hana Sevcikova <hanas@uw.edu>
2022-10-12 16:44:14 -0700

    country name added to popproj5dt

--- data/popproj5dt.R
+++ data/popproj5dt.R
@@ -1,8 +1,7 @@
 # 5-year male, female and total (projected) population summed over ages in long format (as data.table)
 popproj5dt <- local({
-    e <- new.env()
-    load("popproj1dt.rda", envir= e)
-    pop <- data.table::as.data.table(e$popproj1dt)[year %in% seq(2024, 2099, by = 5)]
+    source("popproj1dt.R")
+    pop <- data.table::as.data.table(popproj1dt)[year %in% seq(2024, 2099, by = 5)]
     pop[, year := year + 1]
     pop
 })
