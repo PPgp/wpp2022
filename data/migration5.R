a326e0d106b0d1b8e5996e36b3d8a91c64e462d2
Hana Sevcikova <hanas@uw.edu>
2022-10-12 15:47:02 -0700

    fixed 5-year migration period

--- data/migration5.R
+++ data/migration5.R
@@ -4,5 +4,5 @@ migration5 <- local({
     e <- new.env()
     load("countries.rda", envir= e)
     e$migration5dt <- merge(data.table::as.data.table(migration5dt), e$countries[,c("country_code", "country_order")], by = "country_code", sort = FALSE)
-    as.data.frame(data.table::dcast(e$migration5dt, country_order + country_code + name ~ year, value.var = "mig")[, `:=`(country_order = NULL)][])
+    as.data.frame(data.table::dcast(e$migration5dt, country_order + country_code + name ~ period, value.var = "mig")[, `:=`(country_order = NULL)][])
 })
