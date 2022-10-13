689bd1c3bca0944d1cd569925d4f7dac10716ed2
Hana Sevcikova <hanas@uw.edu>
2022-10-12 17:49:48 -0700

    fixes in wide 5-year datasets

--- data/popproj95u.R
+++ data/popproj95u.R
@@ -2,7 +2,7 @@ popproj95u <- local({
     source('popproj5dt.R', local = TRUE)
     e <- new.env()
     load("countries.rda", envir= e)
-    e$popproj5dt <- merge(data.table::as.data.table(popproj5dt), e$countries, by = "country_code", sort = FALSE)
+    e$popproj5dt <- merge(data.table::as.data.table(popproj5dt), e$countries[,c("country_code", "country_order")], by = "country_code", sort = FALSE)
     as.data.frame(data.table::dcast(e$popproj5dt, country_order + country_code + name ~ year, value.var = "pop_95u")[, `:=`(country_order = NULL)][])
     
     #source('popproj95u5.R')
--- data/popprojLow5.R
+++ data/popprojLow5.R
@@ -3,6 +3,6 @@ popprojLow5 <- local({
     source('popproj5dt.R', local = TRUE)
     e <- new.env()
     load("countries.rda", envir= e)
-    e$popproj5dt <- merge(data.table::as.data.table(popproj5dt), e$countries, by = "country_code", sort = FALSE)
+    e$popproj5dt <- merge(data.table::as.data.table(popproj5dt), e$countries[,c("country_code", "country_order")], by = "country_code", sort = FALSE)
     as.data.frame(data.table::dcast(e$popproj5dt, country_order + country_code + name ~ year, value.var = "pop_low")[, `:=`(country_order = NULL)][])
 })
